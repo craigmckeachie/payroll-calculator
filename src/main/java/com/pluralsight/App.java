a46c1fce35cb070b6d8bd72555041fca6161c60b
Craig McKeachie <craigdmckeachie@gmail.com>
2026-04-22 15:00:21 -0400

    feat: Read employees data from file

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("The employees file could not be read.");
+        }
+
+    }
 }
